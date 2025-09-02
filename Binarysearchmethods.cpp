d81364d231efffcfb3d45d8dca179dbc436cf0e8
Geetik-Sharma <sharmageetik@gmail.com>
2025-09-02 23:37:28 +0530

    new pivot element in array code in binarysearchmethods

--- Binarysearchmethods.cpp
+++ Binarysearchmethods.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 
 // //returning the first and last occurnace of an element is sorted array 
@@ -50,29 +51,103 @@ using namespace std;
 //     cout<<p.first<<","<<p.second<<endl;
 // }
 
-
 //peak index in mountain array
-int peak(vector<int>&arr){
-    //in  ountain array teh array is devided into two parts one is sorted and the other is unsorted 
-    //left part is sorted and right is unsorted since mountain array have only one peak
-    //ex:- [1,2,3,4,5,6,7,6,5,4,3,2,1]
-    int st=0,end=arr.size()-1;
+// int peak(vector<int>&arr){
+//     //in  ountain array teh array is devided into two parts one is sorted and the other is unsorted 
+//     //left part is sorted and right is unsorted since mountain array have only one peak
+//     //ex:- [1,2,3,4,5,6,7,6,5,4,3,2,1]
+//     int st=0,end=arr.size()-1;
+//
+//     while (st<=end)
+//     {
+//         int mid=st+(end-st)/2;
+//         if(arr[mid]<arr[mid+1]){        
+//             st=mid+1;
+//         }
+//         else{
+//             end=mid-1;
+//         }
+//     }
+//     return st;
+// }
+// int main(){
+//     vector<int>arr={0,5,4,3,2};
+//     cout<<"peak index: "<<peak(arr)<<endl;
+//     cout<<endl;
+// }
 
-    while (st<=end)
-    {
+// pivot element in array 
+//in rotated sorted array element from which the element should start is pivot element
+// int main(){
+//     vector<int>arr={7,9,10,11,0,2,3,4};
+//     int st=0,end=arr.size()-1;
+//     int pivot=INT_MAX; 
+//     while(st<end){
+//         int mid=st+(end-st)/2;
+//         if(arr[mid]>=arr[0]){
+//             st=mid+1;
+//         }
+//         else if( arr[mid]<arr[0]){
+//             pivot=min(pivot,arr[mid]);
+//             end=mid;
+//         }
+//     }    
+//     cout<<st<<endl;
+//     cout<<pivot<<endl;
+//     cout<<endl;
+// }
+
+//search in rortated sorted array
+int main(){
+    vector<int>arr={7,9,10,1,2,3};
+    int target;
+    cout<<"enter target \n";
+    cin>>target;
+    int st=0,end=arr.size()-1;
+    while(st<end){
         int mid=st+(end-st)/2;
-        if(arr[mid]<arr[mid+1]){
-           
+        if(arr[mid]>=arr[0]){
             st=mid+1;
         }
-        else{
-            end=mid-1;
+        else if(arr[mid]<arr[0]){
+            end=mid;
         }
     }
-    return st;
-}
-int main(){
-    vector<int>arr={0,5,4,3,2};
-    cout<<"peak index: "<<peak(arr)<<endl;
-    cout<<endl;
+    int pivot=st;
+    end=arr.size()-1;
+    if(target>=arr[pivot] && target<=arr[end]){
+        cout<<"right"<<endl;
+        //target in right side 
+        int st=pivot,end=arr.size()-1;
+        while (st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]==target){
+                cout<<mid<<endl;
+                break;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                st=mid+1;
+            }
+        }
+    }
+    else{
+        //target in right side
+        int st=0,end=pivot-1;
+        while (st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]==target){
+                cout<<mid<<endl;
+                break;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                st=mid+1;
+            }
+        }
+    }
 }
