594879bf14b03fa10dab3a99add5681208fb4a08
Geetik-Sharma <sharmageetik@gmail.com>
2025-09-03 23:13:47 +0530

    new finding square root using binary search with precision is added to binarysearchmeathods

--- Binarysearchmethods.cpp
+++ Binarysearchmethods.cpp
@@ -98,56 +98,97 @@ using namespace std;
 // }
 
 //search in rortated sorted array
-int main(){
-    vector<int>arr={7,9,10,1,2,3};
-    int target;
-    cout<<"enter target \n";
-    cin>>target;
-    int st=0,end=arr.size()-1;
-    while(st<end){
-        int mid=st+(end-st)/2;
-        if(arr[mid]>=arr[0]){
-            st=mid+1;
-        }
-        else if(arr[mid]<arr[0]){
-            end=mid;
-        }
-    }
-    int pivot=st;
-    end=arr.size()-1;
-    if(target>=arr[pivot] && target<=arr[end]){
-        cout<<"right"<<endl;
-        //target in right side 
-        int st=pivot,end=arr.size()-1;
-        while (st<=end){
-            int mid=st+(end-st)/2;
-            if(arr[mid]==target){
-                cout<<mid<<endl;
-                break;
-            }
-            else if(arr[mid]>target){
-                end=mid-1;
-            }
-            else if(arr[mid]<target){
-                st=mid+1;
-            }
-        }
-    }
-    else{
-        //target in right side
-        int st=0,end=pivot-1;
-        while (st<=end){
-            int mid=st+(end-st)/2;
-            if(arr[mid]==target){
-                cout<<mid<<endl;
-                break;
-            }
-            else if(arr[mid]>target){
-                end=mid-1;
-            }
-            else if(arr[mid]<target){
-                st=mid+1;
-            }
-        }
-    }
-}
+// int main(){
+//     vector<int>arr={7,9,10,1,2,3};
+//     int target;
+//     cout<<"enter target \n";
+//     cin>>target;
+//     int st=0,end=arr.size()-1;
+//     while(st<end){
+//         int mid=st+(end-st)/2;
+//         if(arr[mid]>=arr[0]){
+//             st=mid+1;
+//         }
+//         else if(arr[mid]<arr[0]){
+//             end=mid;
+//         }
+//     }
+//     int pivot=st;//making pivot starting element for right sorted array
+//     end=arr.size()-1;// moving end to the end of the array
+//     if(target>=arr[pivot] && target<=arr[end]){
+//         cout<<"right"<<endl;
+//         //target in right side 
+//         int st=pivot,end=arr.size()-1;
+//         while (st<=end){
+//             int mid=st+(end-st)/2;
+//             if(arr[mid]==target){
+//                 cout<<mid<<endl;
+//                 break;
+//             }
+//             else if(arr[mid]>target){
+//                 end=mid-1;
+//             }
+//             else if(arr[mid]<target){
+//                 st=mid+1;
+//             }
+//         }
+//     }
+//     else{
+//         //target in left sorted  side
+//         int st=0,end=pivot-1;
+//         while (st<=end){
+//             int mid=st+(end-st)/2;
+//             if(arr[mid]==target){
+//                 cout<<mid<<endl;
+//                 break;
+//             }
+//             else if(arr[mid]>target){
+//                 end=mid-1;
+//             }
+//             else if(arr[mid]<target){
+//                 st=mid+1;
+//             }
+//         }
+//     }
+// }
+
+//finding square root using binary search
+// float precision(float basenum,int num,int precision_place);
+// int main(){
+//     int num;
+//     cout<<"enter the number\n";
+//     cin>>num;
+//     int st=0,end=num;
+//     int ans=INT_MIN;
+//     while (st<=end)
+//     {
+//         int mid=st+(end-st)/2;
+//         if((mid*mid)>num){
+//             end=mid-1;
+//         }
+//         else if((mid*mid)<num){
+//             ans=max(ans,mid);
+//             st=mid+1;
+//         }
+//         else {
+//             ans=mid;
+//             break;
+//         }
+//     }
+//     int precision_place;
+//     cout<<"ente the number of palace after decimal\n";
+//     cin>>precision_place;
+//     float precise=precision(ans,num,precision_place);
+//     cout<<precise<<endl;
+// }
+// float precision(float basenum,int num,int precision_place){
+//     float add=1;
+//     for(int i=0;i<=precision_place;i++){
+//         add/=10;
+//         while(((basenum+add)*(basenum+add))<num){
+//             basenum+=add;
+//         }
+//     }
+//     return basenum;
+// }
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,51 +1,39 @@
-#include <iostream>
-#include<vector>
-using namespace std;
-
-//returning the first and last occurnace of an element is sorted array
-
-int rightmostposition(vector<int>& arr,int n, int k){
-    //arr is the sorted array n is the number of the elements k is the target element
-    int st=0,end=arr.size()-1;
-    int rightmost=-1;
-    while(st<=end){
-        int mid=st+(end-st)/2;
-        if(arr[mid]<k){
-            st=mid+1;
-        }
-        else if( arr[mid]>k){
-            end=mid-1;
-        }
-        else{
-            rightmost=mid;
-            st=mid+1;
-        }
-    }
-    return rightmost;
-}
-int leftmostposition(vector<int>& arr,int n, int k ){
-     //arr is the sorted array n is the number of the elements k is the target element
-    int st=0,end=arr.size()-1;
-    int leftmost=-1;
-    while(st<=end){
-        int mid=st+(end-st)/2;
-        if(arr[mid]<k){
-            st=mid+1;
-        }
-        else if( arr[mid]>k){
-            end=mid-1;
-        }
-        else{
-            leftmost=mid;
-            end=mid-1;
-        }
-    }
-    return leftmost;
-}
+//finding square root using binary search
+float precision(float basenum,int num,int precision_place);
 int main(){
-    vector<int>arr={1,2,2,2,3,3,3,4,4,4,4,5,5};
-    
-    int target=4;
-    pair<int,int>p={leftmostposition(arr,arr.size(),target),rightmostposition(arr,arr.size(),target)};
-    cout<<p.first<<","<<p.second<<endl;
+    int num;
+    cout<<"enter the number\n";
+    cin>>num;
+    int st=0,end=num;
+    int ans=INT_MIN;
+    while (st<=end)
+    {
+        int mid=st+(end-st)/2;
+        if((mid*mid)>num){
+            end=mid-1;
+        }
+        else if((mid*mid)<num){
+            ans=max(ans,mid);
+            st=mid+1;
+        }
+        else {
+            ans=mid;
+            break;
+        }
+    }
+    int precision_place;
+    cout<<"ente the number of palace after decimal\n";
+    cin>>precision_place;
+    float precise=precision(ans,num,precision_place);
+    cout<<precise<<endl;
+}
+float precision(float basenum,int num,int precision_place){
+    float add=1;
+    for(int i=1;i<=precision_place;i++){
+        add/=10;
+        while(((basenum+add)*(basenum+add))<num){
+            basenum+=add;
+        }
+    }
+    return basenum;
 }
