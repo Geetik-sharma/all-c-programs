67e23b130741a16e107bdd565e293023dc1d815c
Geetik-Sharma <sharmageetik@gmail.com>
2025-09-14 23:40:17 +0530

    dsarunner

---
+++ dsarunner.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1={1,2,3,4,5,6};
+    int position=3;
+    vector<int>reverse(v1.size());
+    for(int i=0;i<v1.size();i++){
+       if(i<=position){
+        reverse[i]=v1[i];
+       }
+    }
+    int end=v1.size()-1;
+    while (position<=end)
+    {
+        reverse[position+1]=v1[end];
+        position++;
+        end--;
+        
+     }
+    
+   for(int i: reverse){
+        cout<<i<<", ";
+    }
+    cout<<endl;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,49 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 int main(){
-    vector<int>pages={10,20,30,40};
-    int books=pages.size();
-    int stu=2;
-    int ans=INT_MAX;
-    if(stu>books){
-        cout<<"not possible"<<endl;
-        return -1;
+    vector<int>v1={1,2,3,4,5,6};
+    int position=3;
+    vector<int>reverse(v1.size());
+    for(int i=0;i<v1.size();i++){
+       if(i<=position){
+        reverse.push_back(v1[i]);
+       }
     }
-    else{
-        int max_pages=0,st=0;
-        for(int i:pages){
-            max_pages+=i;
-        }
-        int end=max_pages;
-        while(st<=end){
-            int mid=st+(end-st)/2;            
-            if(validation(pages,stu,mid)){
-                ans=min(ans,mid);
-                end=mid-1;
-            }
-            else{
-                st=mid+1;
-            }
-        }
+    // int end=v1.size()-1;
+    // while (position<end)
+    // {
+    //     reverse[position+1]=v1[end];
+    //     position++;
+    //     end--;
+        
+    // }
+    
+   for(int i: reverse){
+        cout<<i<<", ";
     }
-    cout<<ans<<" is ans"<<endl;
-}
-bool validation(vector<int>&pages,int stu,int mid){
-    int student=1,pages_alloted=0;
-    for(int i=0;i<pages.size();i++){
-        if(pages[i]>mid){
-            return false;
-        }
-        if(pages_alloted+pages[i]<=mid){
-            pages_alloted+=pages[i];
-        }
-        else{
-            student++;
-            pages_alloted=pages[i];
-        }
-    }
-    if(student<=stu){
-        return true;
-    }
-    else{
-        return false;
-    }
+    cout<<endl;
 }
